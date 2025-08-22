097b73d85097465260e1b7d683ec4f0f15247573
Grealishgit <eugyneehunter@gmail.com>
2025-08-23 01:23:09 +0300

    feat: Update Navbar item classes for consistent styling and improved responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,19 +41,24 @@ const Navbar = () => {
                 </div>
                 <div className='ml-4'>
                     <ul className='md:flex hidden space-x-4'>
-                        <li className={`nav-anim text-white font-quicksand font-semibold cursor-pointer${isActive === 'home' ? ' active' : ''}`} onClick={() => handleScroll('home')}>
+                        <li className={`md:nav-anim text-white font-quicksand font-semibold cursor-pointer
+                            ${isActive === 'home' ? ' active' : ''}`} onClick={() => handleScroll('home')}>
                             Home
                         </li>
-                        <li className={`nav-anim text-white font-quicksand font-semibold cursor-pointer${isActive === 'about' ? ' active' : ''}`} onClick={() => handleScroll('about')}>
+                        <li className={`md:nav-anim text-white font-quicksand font-semibold cursor-pointer
+                            ${isActive === 'about' ? ' active' : ''}`} onClick={() => handleScroll('about')}>
                             About the Festival
                         </li>
-                        <li className={`nav-anim text-white font-quicksand font-semibold cursor-pointer${isActive === 'gallery' ? ' active' : ''}`} onClick={() => handleScroll('gallery')}>
+                        <li className={`md:nav-anim text-white font-quicksand font-semibold cursor-pointer
+                            ${isActive === 'gallery' ? ' active' : ''}`} onClick={() => handleScroll('gallery')}>
                             Fashion Gallery
                         </li>
-                        <li className={`nav-anim text-white font-quicksand font-semibold cursor-pointer${isActive === 'highlights' ? ' active' : ''}`} onClick={() => handleScroll('highlights')}>
+                        <li className={`md:nav-anim text-white font-quicksand font-semibold cursor-pointer
+                            ${isActive === 'highlights' ? ' active' : ''}`} onClick={() => handleScroll('highlights')}>
                             Fashion Highlights
                         </li>
-                        <li className={`nav-anim text-white font-quicksand font-semibold cursor-pointer${isActive === 'insights' ? ' active' : ''}`} onClick={() => handleScroll('insights')}>
+                        <li className={`md:nav-anim text-white font-quicksand font-semibold cursor-pointer
+                            ${isActive === 'insights' ? ' active' : ''}`} onClick={() => handleScroll('insights')}>
                             Cultural Insights
                         </li>
                     </ul>
