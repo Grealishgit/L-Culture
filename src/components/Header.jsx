1bb603cef0abd49c6cfa6bd6b20c75804a0154dd
Grealishgit <eugyneehunter@gmail.com>
2025-08-22 21:29:22 +0300

    feat: Update Header component layout and styles; change font family in CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,13 @@ const Header = () => {
         <div className='flex flex-col absolute top-0 left-0 object-cover w-screen items-center justify-center min-h-screen'
             style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
-            <div>
-                <h1 className='text-white text-8xl font-bold'>BULL FIGHTING</h1>
+            <div className='items-center text-center space-y-2 flex flex-col'>
+                <h1 className='text-white md:text-8xl text-5xl font-bold'>BULL FIGHTING</h1>
+                <p className='text-white text-lg'>A show of elegant Luhya fashion</p>
+
+                <button className='px-3 py-2.5 border cursor-pointer mt-4 text-white border-white rounded'>
+                    Explore the fashion
+                </button>
             </div>
 
         </div>
