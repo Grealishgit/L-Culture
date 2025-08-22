cdd4829a1941311fc788dd156c96378048e2256e
Grealishgit <eugyneehunter@gmail.com>
2025-08-23 00:17:43 +0300

    feat: Enhance Header with motion effects and update App rendering with MotionConfig

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,45 @@
 import React from 'react'
 import bg from '../assets/bg.jpg'
 import TextPressure from './TextPressure';
+import { motion } from 'motion/react'
 
 const Header = () => {
     return (
         <div className='flex flex-col absolute top-0 left-0 object-cover w-screen items-center justify-center min-h-screen'
             style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
-            <div className='items-center text-center space-y-2 flex flex-col'>
-                <h1 className='text-white md:text-8xl text-5xl font-bold'></h1>
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.5 }}
+                className='items-center text-center space-y-2 flex flex-col'>
 
-                <div style={{ position: 'relative', height: '300px', cursor: 'pointer' }}>
-                    <TextPressure
-                        text="FASHION AFRICA!"
-                        flex={true}
-                        alpha={false}
-                        stroke={false}
-                        width={true}
-                        weight={true}
-                        italic={true}
-                        textColor="#ffffff"
-                        strokeColor="#ff0000"
-                        minFontSize={100}
-                    />
-                </div>
-                <p className='text-white text-lg'>A show of elegant African fashion</p>
+                <motion.h1
+                    initial={{ opacity: 0, x: 60 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className='text-white md:text-8xl text-5xl font-bold'>Fashion Africa</motion.h1>
+
+
+                <motion.p
+                    initial={{ opacity: 0, x: 20 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className='text-white text-lg'>A show of elegant African fashion</motion.p>
 
                 <button className='px-3 py-2.5 border cursor-pointer mt-4 text-white border-white rounded'>
                     Explore the fashion
                 </button>
-            </div>
+            </motion.div>
 
             <div className='absolute items-center flex bottom-0 p-4'>
-                <p className='text-white text-center'>A vibrant showcase of African culture and style. Experience the richness of African heritage through fashion.
-                </p>
+                <motion.p
+                    initial={{ opacity: 0, x: 60 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className='text-white text-center'>
+                    A vibrant showcase of African culture and style. Experience the richness of African heritage through fashion.
+                </motion.p>
             </div>
         </div>
     )
